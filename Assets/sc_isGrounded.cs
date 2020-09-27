b7337709dca728cefec77c8f8bd5c6dcc89674a0
Brydon Yao <zhy204@nyu.edu>
2020-09-27 18:13:08 -0400

    Project Finished

---
+++ Assets/sc_isGrounded.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_isGrounded : MonoBehaviour
+{
+    public bool isGrounded;
+
+    void OnTriggerStay2D(Collider2D other){
+        //print("grounded rn");
+        isGrounded = true;
+    }
+
+    void OnTriggerExit2D(){
+        isGrounded = false;
+    }
+}
---
+++ Assets/sc_movement_control.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class sc_movement_control : MonoBehaviour
+{
+    public SpriteRenderer thisSprite;
+    public Rigidbody2D thisRigid2D;
+    public float force = 10;
+    public float jumpForce = 10;
+    public sc_isGrounded groundCheckScript;
+    public bool onPlatform;
+    public float gravityInAir = 2f;
+
+    public Vector3 movementVector;
+
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>();
+    }
+
+    public bool moving;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            thisRigid2D.AddForce(Vector2.right * force * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            if (thisSprite.flipX == false)
+            {
+                thisSprite.flipX = true;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            thisRigid2D.AddForce(-Vector2.right * force * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            if (thisSprite.flipX == true)
+            {
+                thisSprite.flipX = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            if (groundCheckScript.isGrounded){
+                thisRigid2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+          }
+            if (groundCheckScript.isGrounded == true){
+                thisRigid2D.gravityScale = 1;
+            }
+            else {
+                thisRigid2D.gravityScale = gravityInAir;
+            }
+    }
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Mushrooms")){
+
+            Destroy(other.gameObject);
+        }
+    }
+}
